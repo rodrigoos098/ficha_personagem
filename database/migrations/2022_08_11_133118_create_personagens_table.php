abece72f5cafd95bab305dc27911929f18b4cd62
Rodrigo Oliveira Santos <rodrigoos@dcomp.ufs.br>
2022-08-11 10:56:53 -0300

    Criados os atributos da migration personagens

--- database/migrations/2022_08_11_133118_create_personagens_table.php
+++ database/migrations/2022_08_11_133118_create_personagens_table.php
@@ -15,6 +15,14 @@ class CreatePersonagensTable extends Migration
     {
         Schema::create('personagens', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->integer('xp');
+            $table->integer('idade');
+            $table->integer('altura');
+            $table->integer('peso');
+            $table->foreignId('classe_id')->constrained('classes');
+            $table->foreignId('raca_id')->constrained('racas');
+            $table->foreignId('atributo_id')->constrained('atributos');
             $table->timestamps();
         });
     }
