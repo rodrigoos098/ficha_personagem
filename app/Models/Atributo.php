6c5f6aaca52c092be79151d7fcf76f71be16f432
Rodrigo Oliveira Santos <rodrigoos@dcomp.ufs.br>
2022-08-12 09:20:42 -0300

    Corpo das models criados    =?

---
+++ app/Models/Atributo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Atributo extends Model
+{
+    use HasFactory;
+}
