20f0e072ffeefe0943a39d0866c549f15b88940c
Rodrigo Oliveira Santos <rodrigoos@dcomp.ufs.br>
2022-08-12 10:17:44 -0300

    Criados relacionamentos da Model Personagem    =)

--- app/Models/Personagem.php
+++ app/Models/Personagem.php
@@ -2,10 +2,54 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Personagem extends Model
 {
-    use HasFactory;
+    protected $table = 'personagens';
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+
+    ];
+
+    public function classeRelationship(){
+        return $this->belongsTo(Classe::class,'classe_id');
+    }
+
+    public function racaRelationship(){
+        return $this->belongsTo(Raca::class,'raca_id');
+    }
+
+    public function atributoRelationship(){
+        return $this->belongsTo(Atributo::class, 'atributo_id');
+    }
+
+    public function itemUnicoRelationship(){
+        return $this->hasMany(ItemUnico::class, 'personagem_id');
+    }
+
+    public function campanhaRelationship(){
+        return $this->belongsToMany(Campanha::class, 'personagens_has_campanhas', 'personagem_id', 'campanha_id');
+    }
 }
